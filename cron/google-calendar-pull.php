266348e602b461f72ae5f5c0d2f791b27696f545
David Woodruff <mustang2009cobra@gmail.com>
2013-04-11 19:28:12 -0600

    Added connection to DB

--- cron/google-calendar-pull.php
+++ cron/google-calendar-pull.php
@@ -7,6 +7,16 @@ require './Doctrine/Doctrine/Common/ClassLoader.php';
 $classLoader = new ClassLoader('Doctrine', './Doctrine');
 $classLoader->register();
 
+$config = new \Doctrine\DBAL\Configuration();
+
+$connectionParams = array(
+	'dbname' => 'calendar_service',
+	'user' => 'root',
+	'password' => '',
+	'host' => 'localhost',
+	'driver' => 'pdo_mysql'
+);
+$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
 
 //Script goes here
 
