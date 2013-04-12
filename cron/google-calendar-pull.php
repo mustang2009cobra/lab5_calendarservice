cb4b859b2fef3f79cd270f8e0ff92a79baba06d5
David Woodruff <mustang2009cobra@gmail.com>
2013-04-11 19:49:22 -0600

    Trying to get data from calendar API

--- cron/google-calendar-pull.php
+++ cron/google-calendar-pull.php
@@ -7,8 +7,11 @@ require './Doctrine/Doctrine/Common/ClassLoader.php';
 $classLoader = new ClassLoader('Doctrine', './Doctrine');
 $classLoader->register();
 
-$config = new \Doctrine\DBAL\Configuration();
 
+/**
+ * Setup Initial Database Connection
+ */ 
+$config = new \Doctrine\DBAL\Configuration();
 $connectionParams = array(
 	'dbname' => 'calendar_service',
 	'user' => 'root',
@@ -18,8 +21,24 @@ $connectionParams = array(
 );
 $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
 
+
+/**
+ * Query Google Calendar API for each user
+ */
 $users = get_db_users($conn);
-var_dump($users);
+foreach($users as $user){
+	//Make call to Google API
+	$access_token = $user['googleAccessToken'];
+	$ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://www.googleapis.com/calendar/v3/users/me/calendarList/?access_token=$access_token");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    $retData = json_decode($output);
+    var_dump($retData);
+}
 
 
 ////////////////////////////////////
